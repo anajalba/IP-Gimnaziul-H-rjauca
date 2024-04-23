c77ec888b6a1d243ee7e210b51f1faf0fba16431
anajalba <152983479+anajalba@users.noreply.github.com>
2024-04-23 18:42:57 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+new SimpleLightbox({elements: ".galerie-container a"});
+
+new SimpleLightbox({elements: ".orar-imagini a"});
+
+AOS.init({ once: true });
+
+emailjs.init("i4oLjTn066MrORzwp");
+
+function mobileMenu() {
+    var x = document.getElementById("navbar");
+
+    if (x.className === "") {
+        x.className = "mobile";
+    } else {
+        x.className = "";
+    }
+}
+
+function send() {
+    
+    if (document.getElementById('formular').checkValidity() == false) {
+    (document.getElementById('formular').reportValidity() == false)
+    return;
+}
+
+var date = {
+    nume: document.getElementById("nume").value,
+    email: document.getElementById("email").value,
+    mesaj: document.getElementById("mesaj").value
+};
+
+emailjs.send("service_gd95xfb","template_1abjd7u", date)
+    .then(function (raspuns) {
+        alert("Mesajul a fost transmis.")
+        document.getElementById("formular").reset();
+    }, function (error) {
+        alert("Eroare la transmitere. Contactati programatorul.")
+    })
+}
